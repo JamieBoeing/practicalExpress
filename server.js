f05d60c15d929d1f1a5acfc3ed0bd51c0abeec1f
jamie-boeing <bleupaon@outlook.com>
2023-05-22 19:27:12 -0500

    greetings

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(3000, () => {
-    console.log('Yes I am listening')
+// Greetings 
+app.get('/greeting/:name', (req, res) => {
+    const name = req.params.name
+    res.send(`Hello, ${name}! It's so great to see you!`)
+})
+
+app.listen(port, () => {
+    console.log(`Yes I am listening on port ${port}`)
 })
